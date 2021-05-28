8e0ecf7925824b4591a77e820576bcf317484975
Maxime Favier <maxime1.favier@free.fr>
2021-05-28 15:44:53 +0200

    Maxime : update css index + signin

--- views/index.php
+++ views/index.php
@@ -13,78 +13,82 @@
 <?php
 include "views/common/headerIndex.php";
 ?>
-
-<section class="presentation">
-    <p>
-        "Les mesures les plus précises, les appareils les plus fiables"
-    </p>
-</section>
-<section class=corp>
-    <div id="photos">
-        <div id="gallery">
-            <div><a href="#0"><img src="/design/img/1.jpg"></a></div>
-            <div><a href="#1"><img src="/design/img/2.jpg"></a></div>
-            <div><a href="#2"><img src="/design/img/3.jpg"></a></div>
-            <div><a href="#3"><img src="/design/img/4.jpg"></a></div>
+<div id="body">
+    <section class = "presentation">
+        <p>
+            "Les mesures les plus précises, les appareils les plus fiables"
+        </p>
+    </section>
+    <section class = corp>
+        <div id="photos">
+            <div id="gallery">
+                <div><a href="#0"><img src="/design/img/1.jpg"></a></div>
+                <div><a href="#1"><img src="/design/img/2.jpg"></a></div>
+                <div><a href="#2"><img src="/design/img/3.jpg"></a></div>
+                <div><a href="#3"><img src="/design/img/4.jpg"></a></div>
+            </div>
+            <img src="/design/img/1.jpg" id="base">
+            <div id="main">
+                <ul>
+                    <li><img src="/design/img/1.jpg" id="0"></li>
+                    <li><img src="/design/img/2.jpg" id="1"></li>
+                    <li><img src="/design/img/3.jpg" id="2"></li>
+                    <li><img src="/design/img/4.jpg" id="3"></li>
+                </ul>
+            </div>
         </div>
-        <div id="main">
-            <ul>
-                <li><img src="/design/img/1.jpg" id="0"></li>
-                <li><img src="/design/img/2.jpg" id="1"></li>
-                <li><img src="/design/img/3.jpg" id="2"></li>
-                <li><img src="/design/img/4.jpg" id="3"></li>
-            </ul>
+
+
+
+
+        <div class ="Articles">
+            <article>
+                <h2>Infinite measure Partenaire du monde</h2>
+                <p>Constituendi autem sunt qui sint in amicitia fines
+                    et quasi termini diligendi. De quibus tres video
+                    sententias ferri, quarum nullam probo, unam, ut eodem
+                    modo erga amicum adfecti simus, quo erga nosmet ipsos,
+                    alteram, ut nostra in amicos benevolentia illorum erga
+                    nos benevolentiae pariter aequaliterque respondeat, tertiam,
+                    ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
+            </article>
+
+            <article>
+                <h2>Infinite measure nouveau leader du suivi des pilotes</h2>
+                <p>Constituendi autem sunt qui sint in amicitia fines
+                    et quasi termini diligendi. De quibus tres video
+                    sententias ferri, quarum nullam probo, unam, ut eodem
+                    modo erga amicum adfecti simus, quo erga nosmet ipsos,
+                    alteram, ut nostra in amicos benevolentia illorum erga
+                    nos benevolentiae pariter aequaliterque respondeat, tertiam,
+                    ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
+            </article>
+
+            <article>
+                <h2>Patch 5.4.2 d'Innofly</h2>
+                <p>Constituendi autem sunt qui sint in amicitia fines
+                    et quasi termini diligendi. De quibus tres video
+                    sententias ferri, quarum nullam probo, unam, ut eodem
+                    modo erga amicum adfecti simus, quo erga nosmet ipsos,
+                    alteram, ut nostra in amicos benevolentia illorum erga
+                    nos benevolentiae pariter aequaliterque respondeat, tertiam,
+                    ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
+            </article>
+
+            <article>
+                <h2>  Patch 5.4.1 </h2>
+                <p>Constituendi autem sunt qui sint in amicitia fines
+                    et quasi termini diligendi. De quibus tres video
+                    sententias ferri, quarum nullam probo, unam, ut eodem
+                    modo erga amicum adfecti simus, quo erga nosmet ipsos,
+                    alteram, ut nostra in amicos benevolentia illorum erga
+                    nos benevolentiae pariter aequaliterque respondeat, tertiam,
+                    ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
+            </article>
+
         </div>
-    </div>
-
-
-    <div class="Articles">
-        <article>
-            <h2>Infinite measure Partenaire du monde</h2>
-            <p>Constituendi autem sunt qui sint in amicitia fines
-                et quasi termini diligendi. De quibus tres video
-                sententias ferri, quarum nullam probo, unam, ut eodem
-                modo erga amicum adfecti simus, quo erga nosmet ipsos,
-                alteram, ut nostra in amicos benevolentia illorum erga
-                nos benevolentiae pariter aequaliterque respondeat, tertiam,
-                ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
-        </article>
-
-        <article>
-            <h2>Infinite measure nouveau leader du suivi des pilotes</h2>
-            <p>Constituendi autem sunt qui sint in amicitia fines
-                et quasi termini diligendi. De quibus tres video
-                sententias ferri, quarum nullam probo, unam, ut eodem
-                modo erga amicum adfecti simus, quo erga nosmet ipsos,
-                alteram, ut nostra in amicos benevolentia illorum erga
-                nos benevolentiae pariter aequaliterque respondeat, tertiam,
-                ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
-        </article>
-
-        <article>
-            <h2>Patch 5.4.2 d'Innofly</h2>
-            <p>Constituendi autem sunt qui sint in amicitia fines
-                et quasi termini diligendi. De quibus tres video
-                sententias ferri, quarum nullam probo, unam, ut eodem
-                modo erga amicum adfecti simus, quo erga nosmet ipsos,
-                alteram, ut nostra in amicos benevolentia illorum erga
-                nos benevolentiae pariter aequaliterque respondeat, tertiam,
-                ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
-        </article>
-
-        <article>
-            <h2> Patch 5.4.1 </h2>
-            <p>Constituendi autem sunt qui sint in amicitia fines
-                et quasi termini diligendi. De quibus tres video
-                sententias ferri, quarum nullam probo, unam, ut eodem
-                modo erga amicum adfecti simus, quo erga nosmet ipsos,
-                alteram, ut nostra in amicos benevolentia illorum erga
-                nos benevolentiae pariter aequaliterque respondeat, tertiam,
-                ut, quanti quisque se ipse facit, tanti fiat ab amicis.</p>
-        </article>
-
-    </div>
-</section>
+    </section>
+</div>
 
 <?php
 include "views/common/footer.php";
